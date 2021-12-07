0e87dc471c1fff6fdc2c721df46172e037cb91ae
yuanzi1501040205 <ziyuan1501040205@gmail.com>
2021-12-07 14:31:25 -0600

    baseline exp 1

--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import chama
+import csv
 # %% define the grid
 x_grid = np.linspace(0, 100, 100)
 y_grid = np.linspace(0, 100, 100)
@@ -15,22 +16,90 @@ atm = pd.DataFrame({'Wind Direction': [177.98,185.43,185.43,184.68,183.19,182.45
 # %% Initialize the Gaussian plume model
 gauss_plume = chama.simulation.GaussianPlume(grid, source, atm)
 gauss_plume.run()
-signal = gauss_plume.conc
-print(signal.head(5))
-# %% visualize convex hull
-import chama
-chama.graphics.signal_convexhull(signal, scenarios=['S','S'], threshold=1e-100)
+signal_total = gauss_plume.conc
+signal_total = signal_total.drop(columns='S')
+print(signal_total.head(5))
+# %% read leak position
+with open('./data/raw/leak_positions.csv') as leakfile:
+    csvreader = csv.reader(leakfile)
+    rows = []
+    for row in csvreader:
+        rows.append(row)
+leak_positions = []
+for coordinate in rows[1:]:
+    leak_positions.append([int(coordinate[0]), int(coordinate[1])])
+# %% leak positions times 10 for 100 scenarios
+leak_positions = leak_positions*10
+# %% pdf
+with open('./data/raw/leak_pdf.csv') as leakfile:
+    csvreader = csv.reader(leakfile)
+    rows = []
+    for row in csvreader:
+        rows.append(row)
+leak_rates = []
+for rate in rows[1:]:
+    leak_rates.append([float(rate[0]), float(rate[1])])
+
+leak_rate = np.array(leak_rates)[:, 0]
+leak_prob = np.array(leak_rates)[:, 1]
+
+# %% cdf
+import matplotlib.pyplot as plt
+# Compute the CDF
+d_x = leak_rate[1]
+CY = np.cumsum(leak_prob * d_x)
+plt.plot(leak_rate, CY)
+plt.show()
+# %% sample 100 leak rates from PDF of
+import numpy as np
+
+# Choose elements with different probabilities
+sampleLeakRates = np.random.choice(list(leak_rate), 100, p=list(leak_prob)/leak_prob.sum())
+print(sampleLeakRates)
+# %%
+import numpy as np
+
+# Choose elements with different probabilities
+sampleLeakHeights = np.random.choice([0,1,2], 100, p=[0.33,0.33,0.34])
+print(sampleLeakHeights)
+# %% Simulation 100 scenarios
+
+
+for i, (leak_point, leak_h, leak_r)  in enumerate(zip(leak_positions, sampleLeakHeights, sampleLeakRates)):
+    print('simulation: ', i)
+    # source
+    source = chama.simulation.Source(leak_point[0], leak_point[1], leak_h, leak_r)
+    gauss_plume = chama.simulation.GaussianPlume(grid, source, atm)
+    gauss_plume.run()
+    signal = gauss_plume.conc
+    signal_total['S' + str(i)] = signal['S']
+    i = i + 1
+    print(signal_total.head(5))
+# %% save simulation result to csv
+signal_total.to_csv('scenarios.csv', index = False)
 # %% x sections
-chama.graphics.signal_xsection(signal, 'S', threshold=0.001)
-# %% place one stationary sensor
-pos1 = chama.sensors.Stationary(location=(70,70,1))
-det1 = chama.sensors.Point(threshold=1e-5, sample_times=[0,10])
-stationary_pt_sensor = chama.sensors.Sensor(position=pos1, detector=det1)
-# %% extract detection time
+chama.graphics.signal_xsection(signal_total, 'S18', threshold=0.001)
+# %% place 9*9*10*2 (raw*column*height*sensor types) stationary sensor
 sensors = dict()
-sensors['A'] = stationary_pt_sensor
+count =0
+for i in range(9):
+    for j in range(9):
+        for m in range(10):
+            for n in range(2):
+                pos = chama.sensors.Stationary(location=((i+1)*10,(j+1)*10,m))
+                if n == 0:
+                    sensor_threshold = 0.1 #regular sensor
+                elif n == 1:
+                    sensor_threshold = 0.01 #high sensitivity sensor
+                det = chama.sensors.Point(threshold=sensor_threshold, sample_times=list(range(24)))
+                stationary_pt_sensor = chama.sensors.Sensor(position=pos, detector=det)
+                sensors['A_'+str(i)+'_'+str(j)+'_'+str(m)+'_'+str(n)] = stationary_pt_sensor
+                print('count: ', count)
+                count = count + 1
+
+# %% extract detection time
 det_times = chama.impact.extract_detection_times(signal, sensors)
-print('det_times: ', det_times)
+# print('det_times: ', det_times)
 # %% extract the min detect time
 det_time_stats = chama.impact.detection_time_stats(det_times)
 print(det_time_stats)
