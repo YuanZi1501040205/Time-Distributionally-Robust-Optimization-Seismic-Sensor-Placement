5eb184525ac3bbcbcb9158a725702e1322a6d5a2
yuanzi1501040205 <ziyuan1501040205@gmail.com>
2021-12-13 13:49:45 -0600

    update

--- simulation.py
+++ simulation.py
@@ -3,8 +3,8 @@ import pandas as pd
 import chama
 import csv
 # %% define the grid
-x_grid = np.linspace(0, 100, 100)
-y_grid = np.linspace(0, 100, 100)
+x_grid = np.linspace(0, 99, 100)
+y_grid = np.linspace(0, 99, 100)
 z_grid = np.linspace(0, 9, 10)
 grid = chama.simulation.Grid(x_grid, y_grid, z_grid)
 # %% source
@@ -50,21 +50,26 @@ d_x = leak_rate[1]
 CY = np.cumsum(leak_prob * d_x)
 plt.plot(leak_rate, CY)
 plt.show()
+plt.close()
 # %% sample 100 leak rates from PDF of
 import numpy as np
 
 # Choose elements with different probabilities
+np.random.seed(0)
 sampleLeakRates = np.random.choice(list(leak_rate), 100, p=list(leak_prob)/leak_prob.sum())
 print(sampleLeakRates)
-# %%
+# %% scenarios probability
 import numpy as np
 
 # Choose elements with different probabilities
+np.random.seed(0)
 sampleLeakHeights = np.random.choice([0,1,2], 100, p=[0.33,0.33,0.34])
 print(sampleLeakHeights)
-# %% Simulation 100 scenarios
+# %% Simulation 1000 scenarios
 
-
+scenario_worst_impacts = []
+scenario_names = []
+scenario_probs = []
 for i, (leak_point, leak_h, leak_r)  in enumerate(zip(leak_positions, sampleLeakHeights, sampleLeakRates)):
     print('simulation: ', i)
     # source
@@ -72,76 +77,90 @@ for i, (leak_point, leak_h, leak_r)  in enumerate(zip(leak_positions, sampleLeak
     gauss_plume = chama.simulation.GaussianPlume(grid, source, atm)
     gauss_plume.run()
     signal = gauss_plume.conc
-    signal_total['S' + str(i)] = signal['S']
+    scenario_name = 'S' + str(i)
+    signal_total[scenario_name] = signal['S']
     i = i + 1
     print(signal_total.head(5))
+    scenario_worst_impacts.append(24*200)
+    scenario_names.append(scenario_name)
+    scenario_probs.append(1/leak_positions.__len__())
+
+scenario =pd.DataFrame({'Scenario': scenario_names,'Undetected Impact': scenario_worst_impacts, 'Probability': scenario_probs})
 # %% save simulation result to csv
-signal_total.to_csv('scenarios.csv', index = False)
+# signal_total.to_csv('scenarios.csv', index = False)
 # %% x sections
-chama.graphics.signal_xsection(signal_total, 'S18', threshold=0.001)
+# chama.graphics.signal_xsection(signal_total, 'S1', threshold=0.01)
 # %% place 9*9*10*2 (raw*column*height*sensor types) stationary sensor
 sensors = dict()
-count =0
+sensor_names = []
+count = 0
+sensor_costs = []
 for i in range(9):
     for j in range(9):
         for m in range(10):
             for n in range(2):
+                sensor_name = 'A_'+str(i)+'_'+str(j)+'_'+str(m)+'_'+str(n)
+                sensor_names.append(sensor_name)
                 pos = chama.sensors.Stationary(location=((i+1)*10,(j+1)*10,m))
                 if n == 0:
                     sensor_threshold = 0.1 #regular sensor
+                    sensor_cost = 10000
                 elif n == 1:
                     sensor_threshold = 0.01 #high sensitivity sensor
+                    sensor_cost = 100000
+
+                sensor_costs.append(sensor_cost)
                 det = chama.sensors.Point(threshold=sensor_threshold, sample_times=list(range(24)))
                 stationary_pt_sensor = chama.sensors.Sensor(position=pos, detector=det)
-                sensors['A_'+str(i)+'_'+str(j)+'_'+str(m)+'_'+str(n)] = stationary_pt_sensor
+                sensors[sensor_name] = stationary_pt_sensor
                 print('count: ', count)
                 count = count + 1
 
+sensor =pd.DataFrame({'Sensor': sensor_names,'Cost': sensor_costs})
+
+
+
 # %% extract detection time
-det_times = chama.impact.extract_detection_times(signal, sensors)
+det_times = chama.impact.extract_detection_times(signal_total, sensors)
 # print('det_times: ', det_times)
-# %% extract the min detect time
+# %% extract statistic of detection time
 det_time_stats = chama.impact.detection_time_stats(det_times)
-print(det_time_stats)
+# %% extract the min detect time
 min_det_time = det_time_stats[['Scenario','Sensor','Min']]
 min_det_time = min_det_time.rename(columns={'Min':'Impact'})
-print(min_det_time)
-# %% optimization
-# >>> print(min_det_time)
-#   Scenario Sensor  Impact
-# 0       S1      A     2.0
-# 1       S2      A     3.0
-# 2       S3      B     4.0
-# 3       S4      C     1.0
-# 4       S5      D     2.0
-# >>> print(sensor)
-#   Sensor   Cost
-# 0      A  100.0
-# 1      B  200.0
-# 2      C  400.0
-# 3      D  500.0
-# >>> print(scenario)
-#   Scenario  Undetected Impact  Probability
-# 0       S1               50.0         0.15
-# 1       S2              250.0         0.50
-# 2       S3              100.0         0.05
-# 3       S4               75.0         0.20
-# 4       S5              225.0         0.10
-#
-# >>> impactform = chama.optimize.ImpactFormulation()
-# >>> results = impactform.solve(impact=min_det_time, sensor_budget=1000,
-# ...                              sensor=sensor, scenario=scenario,
-# ...                              use_scenario_probability=True,
-# ...                              use_sensor_cost=True)
-#
-# >>> print(results['Sensors'])
-# ['A', 'C', 'D']
-# >>> print(results['Objective'])
-# 7.2
-# >>> print(results['Assessment'])
-#   Scenario Sensor  Impact
-# 0       S1      A     2.0
-# 1       S2      A     3.0
-# 2       S4      C     1.0
-# 3       S5      D     2.0
-# 4       S3   None   100.0
+# %% optimization impact formulation
+impactform = chama.optimize.ImpactFormulation()
+results = impactform.solve(impact=min_det_time, sensor_budget=1000000,
+                           sensor=sensor, scenario=scenario,
+                             use_scenario_probability=True,
+                           use_sensor_cost=True)
+# %%
+print(results['Sensors'])
+print(results['Objective'])
+print(results['Assessment'])
+# %% plot sensor
+result_sensors = dict()
+count = 0
+for name_sensor in results['Sensors']:
+    sensor_x = int(name_sensor.split('_')[1] * 10)
+    sensor_y = int(name_sensor.split('_')[2] * 10)
+    sensor_h = int(name_sensor.split('_')[3])
+    sensor_type = int(name_sensor.split('_')[4])
+    pos = chama.sensors.Stationary(location=(sensor_x,sensor_y,sensor_h))
+    if sensor_type == 0:
+        sensor_threshold = 0.1 #regular sensor
+        sensor_cost = 10000
+    elif sensor_type == 1:
+        sensor_threshold = 0.01 #high sensitivity sensor
+        sensor_cost = 100000
+    det = chama.sensors.Point(threshold=sensor_threshold, sample_times=list(range(24)))
+    stationary_pt_sensor = chama.sensors.Sensor(position=pos, detector=det)
+    result_sensors[name_sensor] = stationary_pt_sensor
+    print('count: ', count)
+    count = count + 1
+# %%
+chama.graphics.sensor_locations(result_sensors)
+# %%
+results['Assessment'].head(52).plot(kind='bar')
+plt.show()
+
